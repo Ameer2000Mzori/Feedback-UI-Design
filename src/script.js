f0ebaba35d48742beca8cb4bc700b62af7105940
Ameer <ameerapex496@gmail.com>
2023-12-12 09:03:47 +0100

    added two types of eventlinser + added comments and function

--- src/script.js
+++ src/script.js
@@ -2,9 +2,26 @@
 var emojisCards = document.querySelectorAll(".emoji-Card");
 // global veribales
 // functions
+var changeStyleFunction = function (emojiEl) {
+    // removing any selected class that an element has
+    emojisCards.forEach(function (el) {
+        el.classList.remove("selected");
+    });
+    // geving the selected class to clicked element
+    emojiEl.classList.add("selected");
+};
 // eventlinsters
+// emojisCards.forEach((emojiEl) => {
+//   emojiEl.addEventListener("mouseover", () => {
+//     changeStyleFunction(emojiEl);
+//     console.log(emojiEl);
+//   });
+// });
+// feel free to use mouseover or click,
+// i prefer click
 emojisCards.forEach(function (emojiEl) {
-    emojiEl.addEventListener("mouseover", function (e) {
-        console.log(e);
+    emojiEl.addEventListener("click", function () {
+        changeStyleFunction(emojiEl);
+        console.log(emojiEl);
     });
 });
--- src/script.ts
+++ src/script.ts
@@ -4,10 +4,31 @@ const emojisCards = document.querySelectorAll(".emoji-Card");
 // global veribales
 
 // functions
+const changeStyleFunction = (emojiEl) => {
+  // removing any selected class that an element has
+  emojisCards.forEach((el) => {
+    el.classList.remove("selected");
+  });
+
+  // geving the selected class to clicked element
+  emojiEl.classList.add("selected");
+};
 
 // eventlinsters
+
+// emojisCards.forEach((emojiEl) => {
+//   emojiEl.addEventListener("mouseover", () => {
+//     changeStyleFunction(emojiEl);
+//     console.log(emojiEl);
+//   });
+// });
+
+// feel free to use mouseover or click,
+// i prefer click
+
 emojisCards.forEach((emojiEl) => {
-  emojiEl.addEventListener("mouseover", (e) => {
-    console.log(e);
+  emojiEl.addEventListener("click", () => {
+    changeStyleFunction(emojiEl);
+    console.log(emojiEl);
   });
 });
