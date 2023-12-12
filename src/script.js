4f25b9bcfbc445f76f79039f231908ed33915d8a
Ameer <ameerapex496@gmail.com>
2023-12-12 08:54:42 +0100

    added comment, selected elements

--- src/script.js
+++ src/script.js
@@ -1,1 +1,10 @@
-console.log("hallo world");
+//selecting elements
+var emojisCards = document.querySelectorAll(".emoji-Card");
+// global veribales
+// functions
+// eventlinsters
+emojisCards.forEach(function (emojiEl) {
+    emojiEl.addEventListener("mouseover", function (e) {
+        console.log(e);
+    });
+});
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,13 @@
-console.log("hallo world");
+//selecting elements
+const emojisCards = document.querySelectorAll(".emoji-Card");
+
+// global veribales
+
+// functions
+
+// eventlinsters
+emojisCards.forEach((emojiEl) => {
+  emojiEl.addEventListener("mouseover", (e) => {
+    console.log(e);
+  });
+});
